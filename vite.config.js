1ab1a420aec8de3ab390999a969058336c1efc55
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-04 21:36:25 +0530

    fix: Web preview does not update on resume.md changes during hot reloading

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import path from 'path'
+import injectResumePlugin from './plugins/vite-plugin-inject-resume'
 
 export default defineConfig({
   root: path.resolve(__dirname, 'themes'),
-  plugins: [UnoCSS()],
+  plugins: [UnoCSS(), injectResumePlugin()],
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
