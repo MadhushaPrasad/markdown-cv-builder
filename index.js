7e7c83cffd019655bd52d6d0ba3a739ea8062528
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-05 14:40:40 +0530

    feat: separate project to proper folder structure

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { injectMarkDown } from './commands/inject-markdown'
-import { generatePDF } from './commands/generate-pdf'
+import { injectMarkDown } from './commands/inject-markdown.js';
+import { generatePDF } from './commands/generate-pdf.js';
 
 // CLI Entry Point
 if (process.argv[1].endsWith('index.js') || process.argv[1].endsWith('markdown-cv-builder.js')) {
-  const args = process.argv.slice(2)
-  const inputPath = args[0] || 'resume.md'
-  const theme = args[1] || 'index'
-  const output = args[2] || 'resume.pdf'
-  const serve = args.includes('--serve')
+  const args = process.argv.slice(2);
+  const inputPath = args[0] || 'resume.md';
+  const theme = args[1] || 'index';
+  const output = args[2] || 'resume.pdf';
+  const serve = args.includes('--serve');
 
   if (serve) {
     injectMarkDown(inputPath, theme)
-    console.log('👉 Now run: pnpm run dev')
+    console.log('👉 Now run: npm run dev (or pnpm run dev) to start Vite dev server');
   }else{
-    generatePDF(inputPath, theme, output)
+    generatePDF(inputPath, theme, output);
   }
 }
